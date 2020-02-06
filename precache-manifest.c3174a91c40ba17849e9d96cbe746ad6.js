ffc1984b68bf4a70238f93845501d7085ddc473a
pun <pjingjai@gmail.com>
2020-02-06 23:19:42 +0700

    Updates

---
+++ precache-manifest.c3174a91c40ba17849e9d96cbe746ad6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1bdb4fc54ec76630e32ef165d4a284",
+    "url": "/wikibrows/index.html"
+  },
+  {
+    "revision": "d3d04158d2a903b10216",
+    "url": "/wikibrows/static/css/2.78096a31.chunk.css"
+  },
+  {
+    "revision": "05a1cab9716d0c8a7c6c",
+    "url": "/wikibrows/static/css/main.fff926c9.chunk.css"
+  },
+  {
+    "revision": "d3d04158d2a903b10216",
+    "url": "/wikibrows/static/js/2.8f3b22a7.chunk.js"
+  },
+  {
+    "revision": "10043301bdecbc14a5b2d3616d0590c0",
+    "url": "/wikibrows/static/js/2.8f3b22a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05a1cab9716d0c8a7c6c",
+    "url": "/wikibrows/static/js/main.c716466b.chunk.js"
+  },
+  {
+    "revision": "1a8c32c796ad3ceea7af",
+    "url": "/wikibrows/static/js/runtime-main.450f1cb4.js"
+  },
+  {
+    "revision": "140f3035346c474d9ee9a4e576c1bf71",
+    "url": "/wikibrows/static/media/enwiktionary.140f3035.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/wikibrows/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "d3aa9e70dc0361a222470afb329ac5ed",
+    "url": "/wikibrows/static/media/wikipedia.d3aa9e70.jfif"
+  }
+]);
